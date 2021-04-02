e4194e88e7bb254939e2521eb6457c69a1f0e049
MarianoBF <mariano.b.fernandez@gmail.com>
2021-04-02 13:20:32 -0300

    adjust initial data

--- initial_data.sql
+++ initial_data.sql
@@ -10,10 +10,12 @@ INSERT INTO productos VALUES (3, 'tostado2', 'Un producto de prueba3', 100.50);
 INSERT INTO productos VALUES (4, 'tostado especial', 'Un producto de prueba4', 200);
 INSERT INTO productos VALUES (5, 'milanesa', 'Un producto de prueba5', 450);
 
-INSERT INTO pedidos VALUES (1, 'pendiente', '2021-03-01 09:20:20', 'tarjeta', 1050, "", 2);
-INSERT INTO pedidos VALUES (2, 'en camino', '2021-03-01 09:20:20', 'tarjeta', 2902, "Enviar servilletas extra", 2);
-INSERT INTO pedidos VALUES (3, 'cancelado', '2021-03-05 09:20:20', 'efectivo', 802, "", 2);
-INSERT INTO pedidos VALUES (4, 'en camino', '2021-03-05 09:20:20', 'tarjeta', 2900, "", 3);
+INSERT INTO pedidos VALUES (1, 'confirmado', '2021-03-01 09:20:20', 'tarjeta', 1050, "", 2);
+INSERT INTO pedidos VALUES (2, 'preparando', '2021-03-01 09:20:20', 'tarjeta', 2902, "Enviar servilletas extra", 2);
+INSERT INTO pedidos VALUES (3, 'entregado', '2021-03-05 09:20:20', 'efectivo', 802, "", 2);
+INSERT INTO pedidos VALUES (4, 'enviando', '2021-03-05 09:20:20', 'tarjeta', 2900, "", 3);
+INSERT INTO pedidos VALUES (5, 'nuevo', '2021-03-01 09:20:20', 'tarjeta', 3000, "", 2);
+INSERT INTO pedidos VALUES (6, 'cancelado', '2021-03-01 09:20:20', 'tarjeta', 1000, "", 2);
 
 INSERT INTO detallePedidos VALUES (2, 1, 2, 1);
 INSERT INTO detallePedidos VALUES (1, 1, 1, 3);
@@ -30,5 +32,6 @@ INSERT INTO detallePedidos VALUES (9, 4, 4, 2);
 INSERT INTO detallePedidos VALUES (10, 4, 1, 10);
 INSERT INTO detallePedidos VALUES (11, 4, 2, 2);
 INSERT INTO detallePedidos VALUES (12, 4, 5, 2);
+
 
 
